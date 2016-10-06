d0ae1affc8db28fc79a37988a9556ddde5082c7f
pqmcgill <pqmcgill@gmail.com>
2016-10-05 21:23:21 -0400

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the official STAMP wedding site!!!!!!!!!</h2>
+        </div>
+        <p className="App-intro">Coming soon!...</p>
+      </div>
+    );
+  }
+}
+
+export default App;
