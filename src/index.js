ee1dd65d2909feced0ccb610f1c8f92c80202a88
pqmcgill <pqmcgill@gmail.com>
2016-10-10 15:56:15 -0400

    added hot module loading for components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import App from './containers/App';
+import App from './containers/App/index';
 import './index.css';
 
 const initialState = {};
@@ -12,5 +12,17 @@ ReactDOM.render(
 	<Provider store={ store }>
 		<App />
 	</Provider>,
-  document.getElementById('root')
+	document.getElementById('root')
 );
+
+if (module.hot) {
+	module.hot.accept('./containers/App/index', () => {
+		const NextApp = require('./containers/App/index').default;
+		ReactDOM.render(
+			<Provider store={ store }>
+				<NextApp />
+			</Provider>,
+			document.getElementById('root')
+		);
+	});
+}
