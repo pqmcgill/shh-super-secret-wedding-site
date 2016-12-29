8d1ec0b02e83ced299021963fc344f3c02bd61bf
pqmcgill <pqmcgill@gmail.com>
2016-12-29 09:59:49 -0500

    fixed broken test

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -22,7 +22,7 @@ export default (state = unauthState, action) => {
 		case types.UNAUTHORIZED:
 		case types.LOGIN_FAILURE:
     case types.LOGOUT:
-			return { nope: 'not gonna pass' };
+			return unauthState;
 
     case types.UPDATE_USER_SUCCESS:
       return {
