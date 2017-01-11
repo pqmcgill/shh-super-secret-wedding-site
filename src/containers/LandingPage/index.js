f7806486d5da95a9efc04d03b7e1206b613e66a0
pqmcgill <pqmcgill@gmail.com>
2017-01-10 20:31:05 -0500

    initial html/css skeleton

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from '../../assets/logo.svg';
 import './LandingPage.css';
+import '../../index.css';
 import LoginForm from '../LoginForm';
 
 export class App extends Component {
-  render() {
+	render() {
 		return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to the official <strong>STAMP</strong> wedding site!!!</h1>
-        </div>
-        <p className="App-intro">Coming soon!...</p>
+			<div className="App">
+				<div className="App-header gutter">
+						<div className="App-title">Sam & Pat</div>
+						<div className="App-rsvp">
+							<button>RSVP</button>
+							<div className="deadline">
+								<div>Deadline to RSVP</div>
+								<div>May 27, 2017</div>
+							</div>
+						</div>
+				</div>
+				<nav className="navBar">
+					<ul className="nav">
+						<li>Important Info</li>
+						<li>Registry</li>
+						<li>Contact</li>
+					</ul>
+				</nav>
 				<LoginForm />
       </div>
     );
