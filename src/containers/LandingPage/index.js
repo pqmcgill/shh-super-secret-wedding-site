cd3148d914c6d2f8941ba37636c708bb98f65d19
pqmcgill <pqmcgill@gmail.com>
2017-01-12 17:14:02 -0500

    initial navigation logic

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from '../../assets/logo.svg';
+import flowerImg from '../../assets/flower1.png';
 import './LandingPage.css';
-import LoginForm from '../LoginForm';
+import '../../index.css';
+
+const contextTypes = {
+  router: React.PropTypes.object
+};
 
 export class App extends Component {
-  render() {
+  constructor (props, context) {
+    super(props, context);
+    this.linkTo = this.linkTo.bind(this);
+  }
+
+  linkTo (path) {
+    this.context.router.transitionTo(path);
+  }
+
+	render() {
 		return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to the official <strong>STAMP</strong> wedding site!!!</h1>
-        </div>
-        <p className="App-intro">Coming soon!...</p>
-				<LoginForm />
+			<div className="App">
+				<div className="App-body gutter">
+          <img className="flowerPic" 
+            src={ flowerImg } 
+            role="presentation"
+          />
+          <div className="info-link" onClick={ this.linkTo.bind(null, '/info') }>
+						<h2>Important Info</h2>
+						<p>Find directions and other important information about what to expect</p>
+					</div>
+					<div className="App-body-rsvp">
+            <button onClick={ this.linkTo.bind(null, '/login')}>RSVP</button>
+						<p>Don't forget to RSVP!</p>
+						<p>The deadline is May 27, 2017</p>
+					</div>
+				</div>
+				<div className="App-footer gutter">
+					Think you know Sam and Pat? Take the quiz to find out!
+				</div>
       </div>
     );
   }
 }
+
+App.contextTypes = contextTypes;
 
 export default connect()(App);
---
+++ src/containers/Registry/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => (
+  <h1>Registry</h1>
+);
