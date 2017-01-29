8401e9d3775bb8f63e1790efad60ba8a3351b32d
pqmcgill <pqmcgill@gmail.com>
2017-01-29 11:50:42 -0500

    registry page

--- src/containers/Registry/Registry.test.js
+++ src/containers/Registry/Registry.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Registry from './';
 
+import styleTestUtil from '../../util/styleTestHelper';
+styleTestUtil();
+
 it('renders without crashing', () => {
 	shallow(<Registry />);
 });
