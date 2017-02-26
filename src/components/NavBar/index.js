088de7fb2bc877888be02a6db3ccca10e169b6d3
pqmcgill <pqmcgill@gmail.com>
2017-02-26 14:58:02 -0500

    removed '/'

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -29,22 +29,22 @@ const NavBar = (props) => {
 			<Row className={ css(styles.nav) }
 				center="xs" start="md,lg">
 				<Col mdOffset={1} lgOffset={1}>
-					<Link className={ css(styles.link) } to='/location'>
+					<Link className={ css(styles.link) } to='location'>
 						Location
 					</Link>
 				</Col>
 				<Col>
-					<Link className={ css(styles.link) } to='/faq'>
+					<Link className={ css(styles.link) } to='faq'>
 						FAQ
 					</Link>
 				</Col>
 				<Col>
-					<Link className={ css(styles.link) } to='/registry'>
+					<Link className={ css(styles.link) } to='registry'>
 						Registry
 					</Link>
 				</Col>
 				<Col>
-					<Link className={ css(styles.link) } to='/contact'>
+					<Link className={ css(styles.link) } to='contact'>
 						Contact
 					</Link>
 				</Col>
